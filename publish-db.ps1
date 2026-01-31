da73ad9f8de7d2563c05d3a296f7838471911811
Oleh Dushuk <redpakon@gmail.com>
2026-01-31 21:27:46 +0200

    Init gulp and publish db

---
+++ publish-db.ps1
@@ -0,0 +1,8 @@
+﻿dotnet msbuild ".\DataCollector.DataBase\DataCollector.DataBase.sqlproj" /t:Build /p:Configuration=Debug
+if ($LASTEXITCODE -ne 0) { throw "Build failed" }
+
+sqlpackage /Action:Publish `
+  /SourceFile:".\DataCollector.DataBase\bin\Debug\DataCollector.DataBase.dacpac" `
+  /TargetServerName:"localhost" `
+  /TargetDatabaseName:"DataCollector_Dev" `
+  /TargetTrustServerCertificate:True
